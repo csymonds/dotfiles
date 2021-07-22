3f34d3706931089ab949dc9bb779470f976f39a9
Christopher Symonds <csymonds@users.noreply.github.com>
2021-07-22 16:45:28 -0700

    Update init.sh
    More sensible apt install and update go version

--- init.sh
+++ init.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-# vim commands
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+# apt
+sudo apt install build-essential cmake \
+libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+libcairo2-dev libx11-dev libxpm-dev libxt-dev \
+python-dev python3-dev curl tmux
+
+# vim 
 mkdir ~/ven
 git clone https://github.com/vim/vim.git ~/ven 
 cd ~/ven/vim
 ./configure --with-features=huge \
             --enable-multibyte \
-	    --enable-rubyinterp=yes \
 	    --enable-pythoninterp=yes \
 	    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
 	    --enable-python3interp=yes \
 	    --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-	    --enable-perlinterp=yes \
-	    --enable-luainterp=yes \
             --enable-gui=gtk2 \
             --enable-cscope \
 	   --prefix=/usr/local
@@ -40,12 +40,11 @@ source .bashrc
 
 # go gimme
 mkdir bin
-sudo apt-get install curl
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
-eval "$(GIMME_GO_VERSION=1.11 gimme)"
+eval "$(GIMME_GO_VERSION=1.15 gimme)"
 echo "# gimme" >> ~/.bash_profile
-echo 'eval "$(GIMME_GO_VERSION=1.11 gimme)"' >> ~/.bash_profile
+echo 'eval "$(GIMME_GO_VERSION=1.15 gimme)"' >> ~/.bash_profile
 
 
 # set up the Go environment
@@ -62,12 +61,5 @@ git submodule update --recursive
 # YCM
 cd ~/.dotfiles/vim/vim.symlink/bundle/YouCompleteMe
 git submodule update --init --recursive
-sudo apt install build-essential cmake python3-dev
 python3 install.py --clang-completer --go-completer
 
-
-# misc
-sudo apt install tmux
-
-
-echo "Don't forget to install the dotfiles!"
